7cd4574545978062ed14dc3903434d6f4728640e
Rob Horton <robhorton@outlook.com>
2018-02-28 14:30:44 -0600

    Adding lots of components and layout.

--- recipe-book/src/app/recipes/recipe-list/recipe-list.component.ts
+++ recipe-book/src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from './recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
@@ -6,6 +7,11 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
+
+  recipes: Recipe[] = [
+    new Recipe('Bacon Wrapped Asparagus', 'Whole-30 Approved, Bacon Wrapped Asparagus', 'https://farm4.staticflickr.com/3150/2473562774_d3761ebdcf.jpg')
+    , new Recipe('Creamy Taco Soup', 'Whole-30 Approved, Creamy Taco Soup', 'https://themovementmenu.com/wp-content/uploads/2017/01/Whole30-Taco-Soup-1-2-2.jpg')
+  ];
 
   constructor() { }
 
---
+++ recipe-book/src/app/shared/ingredient.model.ts
@@ -0,0 +1,9 @@
+export class Ingredient {
+    // public name: string;
+    // public amount: number;
+    // This shortcuts the assignment of the property vars so you don't have to have them declared separately.
+    constructor(public name: string, public amount: number) {
+        // this.name = name;
+        // this.amount = amount;
+    }
+}
